f97d43c02f252bc69675b520f0e3b76eb75fb104
Kosa103 <dotomiro2@gmail.com>
2021-03-04 20:50:26 +0100

    Add multiseries handling and gitignore

--- public/assets/scripts/react.js
+++ public/assets/scripts/react.js
@@ -1,7 +1,15 @@
+
+// ---------------------------------------------------------------------------------------------------------------------
+//                                             VIDEO COMPONENT
+// ---------------------------------------------------------------------------------------------------------------------
+
+
 function Video(props) {
     const season = props.season;
     const episode = convertToTwoDigitsString(props.episode);
-    const videoSource = `./mediaLink/movies/friends/s${season}/Friends${season}x${episode}.mp4`;
+    const seriesName = props.seriesName;
+    const capitalSeriesName = seriesName.charAt(0).toUpperCase() + seriesName.slice(1)
+    const videoSource = `./mediaLink/movies/${seriesName}/s${season}/${capitalSeriesName}${season}x${episode}.mp4`;
     const width = window.innerWidth * 0.8;
     const height = width * 0.57;
     
@@ -30,14 +38,20 @@ function Video(props) {
     );
 }
 
-function App() {
-    const numberOfEpisodesBySeason = [24, 24, 25, 24, 24, 25, 24, 24, 23, 17];
 
+// ---------------------------------------------------------------------------------------------------------------------
+//                                             SERIES COMPONENT
+// ---------------------------------------------------------------------------------------------------------------------
+
+
+function Series({ selectedSeries }) {
     const [selectedEpisode, setSelectedEpisode] = React.useState(1);
     const [selectedSeason, setSelectedSeason] = React.useState(1);
+    
+    const episodesBySeason = selectedSeries.episodes;
 
     function renderSeasonButtons() {
-        const representationArray = new Array(10).fill(0);
+        const representationArray = new Array(Object.keys(selectedSeries.episodes).length).fill(0);
         const seasonButtons = representationArray.map((ep, index) => {
             const season = index + 1;
 
@@ -52,7 +66,7 @@ function App() {
     }
 
     function renderEpisodeLinks() {
-        const representationArray = new Array(numberOfEpisodesBySeason[selectedSeason - 1]).fill(0);
+        const representationArray = new Array(episodesBySeason[selectedSeason]).fill(0);
         const episodeLinks = representationArray.map((ep, index) => {
             const episode = index + 1;
 
@@ -67,8 +81,8 @@ function App() {
     }
 
     return (
-        <div className="main-div">
-            <h1>FRIENDS</h1>
+        <>
+            <h1>{selectedSeries.name}</h1>
             <h2>Season {selectedSeason} Episode {selectedEpisode}</h2>
             <hr />
             <div className="buttons-div">
@@ -81,8 +95,66 @@ function App() {
                 {renderEpisodeLinks()}
             </div>
             <div className="video-div">
-                <Video season={selectedSeason} episode={selectedEpisode} />
+                <Video seriesName={selectedSeries.name.toLowerCase()} season={selectedSeason} episode={selectedEpisode} />
             </div>
+        </>    
+    );
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+//                                             APP COMPONENT
+// ---------------------------------------------------------------------------------------------------------------------
+
+
+
+function App() {
+    const [selectedSeries, setSelectedSeries] = React.useState(series.friends);
+
+    const series = {
+        friends: {
+            name: "FRIENDS",
+            episodes: {
+                1: 24,
+                2: 24,
+                3: 25,
+                4: 24,
+                5: 24,
+                6: 25,
+                7: 24,
+                8: 24,
+                9: 23,
+                10: 17
+            }
+        },
+        joey: {
+            name: "JOEY",
+            episodes: {
+                1: 24,
+                2: 22
+            }
+        }
+    } 
+
+    function renderSeriesButtons() {
+        const series = Object.keys(series).map((seriesName, index) => {
+            return (
+                <div key={`series-key-${index}`}>
+                    <button onClick={() => setSelectedSeries(series[seriesName])} className="button button-episode">{series[seriesName].name}</button>
+                </div>
+            );
+        });
+    }
+
+    return (
+        <div className="main-div">
+            
+            <div className="buttons-div">
+                <h3 className="selector-title">Seasons</h3>
+                {renderSeriesButtons()}
+            </div>
+            <br />
+            <Series selectedSeries={selectedSeries}/>
         </div>
     );
 }
